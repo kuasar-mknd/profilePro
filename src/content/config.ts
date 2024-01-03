501cc98d7b94bc5e4fdee3fa3fd8b395aea13578
Samuel <60432398+kuasar-mknd@users.noreply.github.com>
2024-01-03 21:52:50 +0100

    Update project type and related components

--- src/content/config.ts
+++ src/content/config.ts
@@ -32,8 +32,22 @@ const authorCollection = defineCollection({
     }),
 });
 
+const projectCollection = defineCollection({
+    type: 'content', // v2.5.0 and later
+    schema: ({image}) => z.object({
+        title: z.string(),
+        intro: z.string(),
+        tag: z.string(),
+        image: image().optional(),
+        author: reference('author'),
+        pubDate: z.date(),
+        type: z.string().optional(),
+    }),
+});
+
 export const collections = {
     'blog': blogCollection,
     'author': authorCollection,
     'page': pageCollection,
+    'project': projectCollection,
 };
