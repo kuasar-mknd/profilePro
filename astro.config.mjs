1b1a79fd7c42bc27cb912590d4d9169fdafae358
Samuel <60432398+kuasar-mknd@users.noreply.github.com>
2024-01-16 10:02:16 +0100

    Mise à jour de la configuration Astro : site et base modifiés

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: "https://kuasar-mknd.github.io/profilePro/"
+  site: "https://kuasar-mknd.github.io/",
+  base: "/profilePro/",
 });
