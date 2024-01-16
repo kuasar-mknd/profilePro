2086a7c22bda8dce5504811243acededa9ff78d3
Samuel <60432398+kuasar-mknd@users.noreply.github.com>
2024-01-16 10:29:18 +0100

    Mise à jour des URL du site et des médias sociaux

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), mdx()],
-  site: "https://kuasar-mknd.github.io/",
-  base: "/profilePro/",
+  site: "https://portfolio.kuasar.xyz",
 });
--- src/config.mjs
+++ src/config.mjs
@@ -5,7 +5,7 @@ export default {
     defaultOgImage: "./defaultog.jpg",
     latestPosts: 3,
     postsPerPage: 3,
-    url: "https://kuasar-mknd.github.io/profilePro/",
+    url: "https://kuasar.xyz",
     social: {
         'dribbble': false,
         'facebook': false,
