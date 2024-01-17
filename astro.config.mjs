772eedc4930eae25d4c1ed94d3092d1825310855
Samuel <samuel@mknd-records.com>
2024-01-17 12:33:25 +0100

    Ajouter le module sitemap à la configuration Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
-  site: "https://portfolio.kuasar.xyz",
+  integrations: [tailwind(), mdx(), sitemap()],
+  site: "https://portfolio.kuasar.xyz"
 });
