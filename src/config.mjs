e0b14cbc27bc093eb0e20259f2b8b2c46d6ddd0d
Samuel <samuel@mknd-records.com>
2025-11-27 15:54:29 -0500

    feat: add LocalBusiness schema, geo-targeting meta tags, and location details for improved local SEO

--- src/config.mjs
+++ src/config.mjs
@@ -1,21 +1,23 @@
 export default {
-    title: "Samuel Dulex",
-    titleSuffix: " | Connecter, Communiquer, Captiver",
-    description: "L'Art de transformer chaque événement en Histoire.",
-    defaultOgImage: "./defaultog.jpg",
-    latestPosts: 20,
-    postsPerPage: 20,
-    url: "https://kuasar.xyz",
-    social: {
-        'dribbble': false,
-        'facebook': false,
-        'github': 'https://github.com/kuasar-mknd',
-        'instagram': 'https://www.instagram.com/kuasar.mknd',
-        'mastodon': false,
-        'pinterest': false,
-        'skype': false,
-        'twitter': false,
-        'youtube': 'https://www.youtube.com/channel/UCLPJkiQD8VAJSV3k3gSml4w',
-
-    }
-}
+  title: "Samuel Dulex",
+  titleSuffix: " | Connecter, Communiquer, Captiver",
+  description:
+    "Créateur de contenu visuel et développeur web à Lausanne, Suisse romande. Vidéo, photographie, développement fullstack et stratégie digitale.",
+  location: "Lausanne, Suisse romande",
+  catchmentArea: ["Lausanne", "Vaud", "Suisse romande", "Genève", "Fribourg"],
+  defaultOgImage: "./defaultog.jpg",
+  latestPosts: 20,
+  postsPerPage: 20,
+  url: "https://kuasar.xyz",
+  social: {
+    dribbble: false,
+    facebook: false,
+    github: "https://github.com/kuasar-mknd",
+    instagram: "https://www.instagram.com/kuasar.mknd",
+    mastodon: false,
+    pinterest: false,
+    skype: false,
+    twitter: false,
+    youtube: "https://www.youtube.com/channel/UCLPJkiQD8VAJSV3k3gSml4w",
+  },
+};
