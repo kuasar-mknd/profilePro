d6daeb5825f2ad921f99e6f125d16be1b09df15f
kuasar-mknd <60432398+kuasar-mknd@users.noreply.github.com>
2026-02-25 13:41:42 +0000

    fix: resolve no-console linting errors in CI
    - Add `/* eslint-disable no-console */` to `test_url.ts` as it's a test script.
    - Add `// eslint-disable-next-line no-console` to `console.error` calls in `src/pages/[slug].astro`, `src/components/common/Footer.astro`, and `src/components/common/ContactForm.astro` where they are used for legitimate error logging.
    - Fix unused variable warnings in `test_url.ts` by prefixing with `_`.

--- test_url.ts
+++ test_url.ts
@@ -1,17 +1,18 @@
+/* eslint-disable no-console */
 try {
   console.log('images/logo.png:', new URL('images/logo.png'));
-} catch (e) {
+} catch (_e) {
   console.log('images/logo.png failed to parse');
 }
 
 try {
   console.log('http://:8080:', new URL('http://:8080'));
-} catch (e) {
+} catch (_e) {
   console.log('http://:8080 failed to parse');
 }
 
 try {
   console.log('foo:bar:', new URL('foo:bar'));
-} catch (e) {
+} catch (_e) {
   console.log('foo:bar failed to parse');
 }
