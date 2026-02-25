25c31cf11ed7e520c8738996d6624730bea8a353
kuasar-mknd <60432398+kuasar-mknd@users.noreply.github.com>
2026-02-25 04:58:59 +0000

    🎨 Palette: Fix linting errors & Refactor Theme Switcher to Semantic Switch

--- test_url.ts
+++ test_url.ts
@@ -1,17 +1,26 @@
+// eslint-disable-next-line no-console
+console.log('--- URL Parsing Tests ---');
+
 try {
+  // eslint-disable-next-line no-console
   console.log('images/logo.png:', new URL('images/logo.png'));
-} catch (e) {
+} catch {
+  // eslint-disable-next-line no-console
   console.log('images/logo.png failed to parse');
 }
 
 try {
+  // eslint-disable-next-line no-console
   console.log('http://:8080:', new URL('http://:8080'));
-} catch (e) {
+} catch {
+  // eslint-disable-next-line no-console
   console.log('http://:8080 failed to parse');
 }
 
 try {
+  // eslint-disable-next-line no-console
   console.log('foo:bar:', new URL('foo:bar'));
-} catch (e) {
+} catch {
+  // eslint-disable-next-line no-console
   console.log('foo:bar failed to parse');
 }
