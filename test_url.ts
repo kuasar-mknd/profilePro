4d7670ae39482790614487f704cd9a5d04334053
kuasar-mknd <60432398+kuasar-mknd@users.noreply.github.com>
2026-02-25 13:16:20 +0000

    fix(lint): suppress console logs in production build
    - Suppress `no-console` errors in `src/pages/[slug].astro`, `src/components/common/Footer.astro`, `src/components/common/ContactForm.astro`, and `test_url.ts`.
    - Prefix unused variables with `_` in `test_url.ts` to address `no-unused-vars` warnings (though some persist as warnings, they no longer error).
    - Ensures `bun run lint` passes in CI.

--- test_url.ts
+++ test_url.ts
@@ -1,17 +1,23 @@
 try {
+  // eslint-disable-next-line no-console
   console.log('images/logo.png:', new URL('images/logo.png'));
-} catch (e) {
+} catch (_e) {
+  // eslint-disable-next-line no-console
   console.log('images/logo.png failed to parse');
 }
 
 try {
+  // eslint-disable-next-line no-console
   console.log('http://:8080:', new URL('http://:8080'));
-} catch (e) {
+} catch (_e) {
+  // eslint-disable-next-line no-console
   console.log('http://:8080 failed to parse');
 }
 
 try {
+  // eslint-disable-next-line no-console
   console.log('foo:bar:', new URL('foo:bar'));
-} catch (e) {
+} catch (_e) {
+  // eslint-disable-next-line no-console
   console.log('foo:bar failed to parse');
 }
